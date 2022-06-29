2a73b561d60f17920a7c5045dcbe110b8da9eab9
ThomasCode92 <thomascode92@hotmail.com>
2022-06-29 13:00:36 +0200

    Adding our First Component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PostCreateComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],

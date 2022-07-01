76d4c215b379674bc6535dd80283fb33b2c45965
ThomasCode92 <thomascode92@hotmail.com>
2022-07-01 08:32:48 +0200

    Outputting Posts

---
+++ frontend/src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css'],
+})
+export class PostListComponent {
+  posts = [
+    { title: 'First Post', content: "This is the first posts's content" },
+    { title: 'Second Post', content: "This is the second posts's content" },
+    { title: 'Third Post', content: "This is the third posts's content" },
+  ];
+}

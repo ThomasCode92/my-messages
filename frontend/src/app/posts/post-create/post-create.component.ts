c51ad390dd2cbcee48c28498f413edbdc9beb7f7
ThomasCode92 <thomascode92@hotmail.com>
2022-07-01 15:30:14 +0200

    Adding Forms

--- frontend/src/app/posts/post-create/post-create.component.ts
+++ frontend/src/app/posts/post-create/post-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 import { Post } from '../post.model';
 
@@ -13,11 +14,14 @@ export class PostCreateComponent {
 
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) return;
+
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent,
+      title: form.value.title,
+      content: form.value.content,
     };
+
     this.postCreated.emit(post);
   }
 }

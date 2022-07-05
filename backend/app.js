86c4812df473f41bb56cffbd02bb44ddf457a34b
ThomasCode92 <thomascode92@hotmail.com>
2022-07-05 12:57:30 +0200

    Adding the POST Backend Point

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
@@ -16,7 +18,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/api/posts', (req, res, next) => {
+app.get('/api/posts', (req, res, next) => {
   const posts = [
     {
       id: 'abc123',
@@ -35,7 +37,14 @@ app.use('/api/posts', (req, res, next) => {
     },
   ];
 
-  res.status(200).json({ message: 'Posts fetched succesfully', posts });
+  res.status(200).json({ message: 'Posts fetched succesfully!', posts });
+});
+
+app.post('/api/posts', (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+
+  res.status(201).json({ message: 'Post added succesfully!', post });
 });
 
 module.exports = app;

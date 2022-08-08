c5268aae0c00f6489fb8d876c5c84cc779241784
ThomasCode92 <thomascode92@hotmail.com>
2022-08-08 12:48:29 +0200

    Creating the User Model

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const postsRoutes = require('./routes/posts.routes');
+const userRoutes = require('./routes/user.routes');
 
 const app = express();
 
@@ -36,6 +37,7 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/api/user', userRoutes);
 app.use('/api/posts', postsRoutes);
 
 module.exports = app;

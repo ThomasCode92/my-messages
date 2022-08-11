140ab072a7833df141cf14ecb862bbbfd7a3680d
ThomasCode92 <thomascode92@hotmail.com>
2022-08-11 12:45:44 +0200

    Adding Route Guards

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,17 +5,27 @@ import { LoginComponent } from './auth/login/login.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGaurd } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: PostListComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'create', component: PostCreateComponent },
-  { path: 'edit/:postId', component: PostCreateComponent },
+  {
+    path: 'create',
+    component: PostCreateComponent,
+    canActivate: [AuthGaurd],
+  },
+  {
+    path: 'edit/:postId',
+    component: PostCreateComponent,
+    canActivate: [AuthGaurd],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
+  providers: [AuthGaurd],
 })
 export class AppRoutingModule {}
---
+++ frontend/src/app/auth/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthGaurd implements CanActivate {
+  constructor(private router: Router, private authService: AuthService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    const isAuth = this.authService.isAuthenticated;
+
+    if (!isAuth) {
+      this.router.navigate(['/login']);
+    }
+
+    return isAuth;
+  }
+}

eede5e6621325fc7cb02b6b62514a00c29ee7b2b
ThomasCode92 <thomascode92@hotmail.com>
2022-08-17 12:45:59 +0200

    Splitting the App Into Feature Modules

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AngularMaterialModule } from './angular-material.module';
 
+import { PostsModule } from './posts/posts.module';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ErrorComponent } from './error/error.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './auth/login/login.component';
-import { PostCreateComponent } from './posts/post-create/post-create.component';
-import { PostListComponent } from './posts/post-list/post-list.component';
+
 import { SignupComponent } from './auth/signup/signup.component';
 import { AuthInterceptor } from './auth/auth.interceptor';
 import { ErrorInterceptor } from './error.interceptor';
@@ -23,8 +24,6 @@ import { ErrorInterceptor } from './error.interceptor';
     ErrorComponent,
     HeaderComponent,
     LoginComponent,
-    PostCreateComponent,
-    PostListComponent,
     SignupComponent,
   ],
   imports: [
@@ -32,9 +31,9 @@ import { ErrorInterceptor } from './error.interceptor';
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule,
     AppRoutingModule,
     AngularMaterialModule,
+    PostsModule,
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
